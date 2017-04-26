e5c4819cd307a3b2e84a017e9e8b17c8677c31a7
ingelieur <ingelieur@gmail.com>
2017-04-27 01:03:00 +0700

    almost there

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,28 +1,39 @@
-var sys = require('sys');
+//var sys = require('sys');
 const fs = require('fs')
 
-// actual conversion code starts here
 function words_in_a_file(filename, limit) {
   let contents = fs.readFileSync(filename).toString();
   let arrContents = contents.match(/[a-z]+/gi);
   let countWords = {};
+  let result = "";
 
-  /*
+  // count occurences for each word
   arrContents.map(key => {
     (!countWords.hasOwnProperty(key)) ? countWords[key] = 1 : countWords[key] += 1;
   });
+
+  // sort the occurences
   let arrCountWords = Object.keys(countWords).map(key => {
     return {
       word: key,
       total: countWords[key]
     }
   });
-  arrCountWords.sort((a,b) => b.total-a.total);*/
+  arrCountWords.sort((a,b) => b.total-a.total);
 
+  // print the result
+  for (let i=0; i<limit; i++) {
+    result += "\'"+arrCountWords[i].word+"\': "+arrCountWords[i].total+" occurences\n";
+    //console.log(arrCountWords[i].word);
+    //console.log(arrCountWords[i].total);
+    //result[arrCountWords[i].word] = arrCountWords[i].total;
+  }
+
+  return result;
 
 }
 
-console.log(words_in_a_file('source.txt',3));
+console.log(words_in_a_file('source.txt',30));
 module.exports = {
   words_in_a_file: words_in_a_file
 }
