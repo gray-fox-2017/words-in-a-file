c4c869d1220c2787dc2b36d82bea37cfcdccff2a
ingelieur <ingelieur@gmail.com>
2017-04-27 00:12:05 +0700

    half done

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -3,8 +3,16 @@ const fs = require('fs')
 
 // actual conversion code starts here
 function words_in_a_file(filename, limit) {
+  let contents = fs.readFileSync(filename).toString();
+  let arrContents = contents.match(/[a-z]+/gi);
+  let countWords = {};
+  arrContents.forEach(key => {
+    (!countWords[key]) ? countWords[key] = 1 : countWords[key] += 1;
+  });
+
 }
 
+console.log(words_in_a_file('source.txt',3));
 module.exports = {
   words_in_a_file: words_in_a_file
 }
