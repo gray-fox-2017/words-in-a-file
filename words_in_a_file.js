943bd0d1551f90d6222bb50609c7b35e13f2e793
ingelieur <ingelieur@gmail.com>
2017-04-27 01:30:04 +0700

    almoooooost

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -2,8 +2,10 @@
 const fs = require('fs')
 
 function words_in_a_file(filename, limit) {
+  const conjuctions = ['a','after','also','although','an','and','are','as','at','because','before','between','both','but','by','either','for','from','if','in','is','it','nor','of','on','or','so','that','the','these','though','to','was','were','which','who','whom','whose','with','yet','ref'];
   let contents = fs.readFileSync(filename).toString();
-  let arrContents = contents.match(/[a-z]+/gi);
+  let unFilteredContents = contents.match(/[a-z]+/gi);
+  let arrContents = unFilteredContents.filter(key => conjuctions.indexOf(key.toLowerCase()) === -1);
   let countWords = {};
   let result = "";
 
